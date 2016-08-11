a0f9768c84e46be6989aaf1ef278433f00202cde
Chris Carlson <chrisc@rationalagency.com>
2016-08-11 12:11:40 -0700

    building out table, adding basic styles, colors

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -1,6 +1,19 @@
-var app = angular.module('DrummingApp', []);
+var app = angular.module('DrummingApp', ['ui.bootstrap']);
 
 app.controller('MainCtrl', ['$scope', function($scope) {
-	$scope.test = 'TEST';
+	
+	var getRandomNumber = function() {
+		return Math.ceil(Math.random() * 5);
+	}
+
+	$scope.getRandomArray = function() {
+		$scope.arr = [];
+
+		for(var i = 0; i < 8; i++) {
+			$scope.arr.push(getRandomNumber());
+		}
+		return $scope.arr;
+	}
+
 }]);
 
